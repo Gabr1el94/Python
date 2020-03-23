379a566ad7faffa661223086ef947d4331c3e923
gabr1el94 <gabrielsoares.dev@gmail.com>
2020-03-23 04:19:02 -0300

    velocidade e volume

--- reconhecimento-voz/pyttsX/app.py
+++ reconhecimento-voz/pyttsX/app.py
@@ -1,13 +1,20 @@
 import pyttsx
 
 test = pyttsx.init()
-gender = 'male'
-#Voice
-voice = 'brazil'
 
+#Speed
+test.setProperty('rate', 50)
+
+#Volume
+test.setProperty('volume',0.10)
+
+#Voice
+gender = 'male'
+voice = 'brazil'
 if gender == 'female':
     voice += '+f4'
 test.setProperty('voice', voice )
+
 #Speak
 test.say('Oi Gabriel, seja bem-vindo!')
 
