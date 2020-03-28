352a241e23cf2c26c5ad9df16ab946fa702d1124
gabr1el94 <gabrielsoares.dev@gmail.com>
2020-03-28 17:25:21 -0300

    Abrir ação por comando de voz

---
+++ reconhecimento-voz/sintext-fala/comando-voz/agenda.py
@@ -0,0 +1,16 @@
+import speech_recognition as sr 
+import os
+
+recon = sr.Recognizer()
+with sr.Microphone() as source:
+    print("Olá Usuário, Deseja abrur sua agenda?")
+    audio = recon.listen(source)
+
+resposta = recon.recognize_google(audio, language="pt-br")
+print(resposta.upper()+"!")
+
+if resposta == 'sim':
+    print("Abrindo Agenda...")
+    os.system(r"gedit < Direct >")
+elif resposta == 'não':
+    print("Solicitação negada!")
