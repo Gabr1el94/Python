efe82717ec47be892b422deb593f709418d1ba91
gabr1el94 <gabrielsoares.dev@gmail.com>
2020-04-02 11:41:30 -0300

    capturando username e fechar o programa

--- automacao-bot/chatbot/app.py
+++ automacao-bot/chatbot/app.py
@@ -2,25 +2,44 @@
 #Caso falte o módulo na hora de iniciar: pip install chatterbot_corpus
 from chatterbot.trainers import ListTrainer
 from chatterbot import ChatBot
+from datetime import datetime
+import os
+
+def horario_para_texto(horario):
+    if horario < 12 :
+        return 'Bom dia'
+    elif horario < 18 :
+        return 'Boa tarde'
+    else:
+        return 'Boa noite'
 
 bot = ChatBot('Ron Obvious')
+date = datetime.now()
+trainer = ListTrainer(bot)
 
-conversation = ['oi','olá', 
-'olá,bom dia',
-'bom dia, como vai?',
-'estou bem' ]
+msgHorario =  horario_para_texto(date.hour)
+
+conveW = ['oi',
+'olá, '+os.getlogin()+"  "+horario_para_texto(date.hour),
+ horario_para_texto(date.hour),'como vai?',
+'estou bem','Que ótimo!']
 
 convFilmes = ['Que filmes que você gosta?',
-'eu gosto de Resident Evil']
+'eu gosto do "Os Vingadores"']
 
-trainer = ListTrainer(bot)
-trainer.train(conversation)
+convClosed = ['fechar o programa',
+'Fechando e até mais :) ...']
+
+trainer.train(conveW)
 trainer.train(convFilmes)
+trainer.train(convClosed)
 
 while True:
     quest = input('Você: ')
     response = bot.get_response(quest)
     if float(response.confidence) > 0.5:
         print('Bot:', response)
+        if quest == 'fechar o programa' : 
+            break
     else:
         print('Bot: Não entendi!')
